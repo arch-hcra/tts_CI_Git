b00b4ed36f74d5a30f0893db84817f819be167fb
archhcra <bomcom@bk.ru>
2025-09-28 19:05:41 +0300

    update_app_2

--- app.py
+++ app.py
@@ -1,25 +1,11 @@
 from flask import Flask
-from flask_httpauth import HTTPBasicAuth
-from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
-auth = HTTPBasicAuth()
-
-users = {
-    "admin": generate_password_hash("admin"),
-    "user": generate_password_hash("user")
-}
-
-@auth.verify_password
-def verify_password(username, password):
-    if username in users and check_password_hash(users[username], password):
-        return username
 
 @app.route('/')
-@auth.login_required
 def hello():
     return 'Hello, protected World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8078)
+    app.run(host='0.0.0.0', port=8078, debug=True)
 
