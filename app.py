e17c22343c0302e1766285ff1e1410c655449aa4
archhcra <bomcom@bk.ru>
2025-09-29 22:25:55 +0300

    testing_new_code

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, protected World!'
+    return 'Hello, my Flask in kube!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8078, debug=True)
